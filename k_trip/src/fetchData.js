fab4519a19eeb09b9947f5325bdbe4af6f732380
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2024-05-20 13:46:39 +0900

    css 파일 추가

--- k_trip/src/fetchData.js
+++ k_trip/src/fetchData.js
@@ -1,31 +1,32 @@
 import axios from "axios";
 
 const fetchData = async (URL, setData, setError, setLoading, areaCode) => {
-    try {
-        setError(null);
-        setData(null);
-        setLoading(true);
+  try {
+    setError(null);
+    setData(null);
+    setLoading(true);
 
-        const params = {
-            numOfRows:'50',
-            MobileOS: 'WIN',
-            MobileApp: 'K-trip',
-            _type: 'json',
-            serviceKey: '본인의 서비스키 입력'
-        };
+    const params = {
+      numOfRows: "50",
+      MobileOS: "WIN",
+      MobileApp: "K-trip",
+      _type: "json",
+      serviceKey:
+        "B4FraEdNAEHerMG6ZQUi5OXCzio/QJ4IRx9rOOz7+eiPBh4L8pX4XAygutNaYnOoL+D8vS/3qZ53efN6daHZ/g==",
+    };
 
-        if (areaCode) {
-            params.areaCode = areaCode;
-        }
-        const response = await axios.get(URL, { params });
-
-        const dataArray = Object.values(response.data.response.body.items.item);
-        setData(dataArray);
-        console.log(dataArray);
-    } catch(e) {
-        setError(e);
+    if (areaCode) {
+      params.areaCode = areaCode;
     }
-    setLoading(false);
+    const response = await axios.get(URL, { params });
+
+    const dataArray = Object.values(response.data.response.body.items.item);
+    setData(dataArray);
+    console.log(dataArray);
+  } catch (e) {
+    setError(e);
+  }
+  setLoading(false);
 };
 
 export default fetchData;
--- k_trip/src/index.js
+++ k_trip/src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/reset.css";
+import "./styles/index.css";
+import reportWebVitals from "./reportWebVitals";
 import Navbar from "./Navbar";
 import Routing from "./Routing";
 
-import Modal from 'react-modal';
+import "./styles/nav-bar.css";
 
-Modal.setAppElement('#root');
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 //Nav bar
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <React.StrictMode>
-        <Navbar/>
-        <Routing/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Navbar />
+    <Routing />
+  </React.StrictMode>
 );
 
 reportWebVitals();
