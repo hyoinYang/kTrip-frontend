4e5fb4324539a0732b74386d252c5c9d8a72bb55
박진영 <am030813@naver.com>
2024-06-22 22:46:04 +0900

    FIX: 하위 행정구역 선택 시 추천 여행지 페이지로 전환

--- k_trip/src/modals/AreaModal.js
+++ k_trip/src/modals/AreaModal.js
@@ -1,54 +1,56 @@
 import React, { useState, useEffect } from "react";
 import Modal from "react-modal"; // react-modal import
-import { StyleSheet } from "react-native";
 import fetchData from "../fetchData";
 import "./modalStyles/areamodal.css";
 
-const AreaModal = ({ isOpen, onClose }) => {
+const AreaModal = ({ isOpen, onClose, onSelectArea }) => {
     const [page, setPage] = useState(1);
-    const [data, setData] = useState([]); // Initialize data as an empty array
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    const [selectedAreaCode, setSelectedAreaCode] = useState(null); // State to store selected area code
-    const [selectedSigunguCode, setSelectedSigunguCode] = useState(null);
+    const [selectedAreaCode, setSelectedAreaCode] = useState(null);
+
+
     const handleCloseModal = () => {
         setPage(1);
-        setSelectedAreaCode(null); // Clear selected area code when modal closes
+        setSelectedAreaCode(null);
         onClose();
     };
 
     const handleNextPage = () => {
-        setPage((prevPage) => prevPage + 1); // Increase page number
+        setPage((prevPage) => prevPage + 1);
         fetchData('trip/area', setData, setError, setLoading, { areacode: selectedAreaCode, pageno: page + 1 });
     };
 
     const handlePrevPage = () => {
-        setPage((prevPage) => Math.max(prevPage - 1, 1)); // Decrease page number but not below 1
+        setPage((prevPage) => Math.max(prevPage - 1, 1));
         fetchData('trip/area', setData, setError, setLoading, { areacode: selectedAreaCode, pageno: page - 1 });
     };
 
     const handleSelectArea = (code) => {
         if (selectedAreaCode === null) {
-            setSelectedAreaCode(code); // Update selected area code
-            fetchData('trip/area', setData, setError, setLoading, { areacode: code, pageno: 1 }); // Fetch data for selected area
+            setSelectedAreaCode(code);
+            fetchData('trip/area', setData, setError, setLoading, { areacode: code, pageno: 1 });
         } else {
-            setSelectedSigunguCode(code); // Update selected sigungu code
-            fetchData('trip/area', setData, setError, setLoading, { areacode: selectedAreaCode, sigungucode: code, pageno: 1 }); // Fetch data for selected sigungu within selected area
+            onSelectArea(selectedAreaCode, code);
+            console.log({selectedAreaCode, code});
+            setSelectedAreaCode(null);
         }
-        console.log({selectedAreaCode, selectedSigunguCode});
     };
 
 
     useEffect(() => {
+        fetchData('trip/area', setData, setError, setLoading, { pageno: 1 });
         if (isOpen) {
-            fetchData('trip/area', setData, setError, setLoading, { pageno: 1 });
+            setPage(1);
+            setSelectedAreaCode(null);
         }
     }, [isOpen]);
 
     return (
         <Modal
             isOpen={isOpen}
-            onRequestClose={handleCloseModal} // 모달 닫힐 때 페이지 초기화 함수를 호출
+            onRequestClose={handleCloseModal}
             contentLabel="Example Modal"
             style={modalStyle}
             ariaHideApp={false}
@@ -89,14 +91,14 @@ const AreaModal = ({ isOpen, onClose }) => {
     );
 };
 
-const modalStyle = StyleSheet.create({
+const modalStyle = {
     overlay: {
         position: "fixed",
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: "rgba(0, 0, 0, 0.5)", // 배경에 흐릿한 효과를 줄 때 사용합니다.
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
     },
     content: {
         position: "absolute",
@@ -106,10 +108,10 @@ const modalStyle = StyleSheet.create({
         maxWidth: "80%",
         maxHeight: "80%",
         overflow: "auto",
-        backgroundColor: "white", // 모달의 배경색을 지정합니다.
+        backgroundColor: "white",
         borderRadius: "10px",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // 그림자 효과를 추가할 수 있습니다.
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
     },
-});
+};
 
 export default AreaModal;
