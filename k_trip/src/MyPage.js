9e1ce5f3475633be54e389f8a43635cc0814a3cf
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2024-06-23 19:33:01 +0900

    마이페이지 구현완료

--- k_trip/src/MyPage.js
+++ k_trip/src/MyPage.js
@@ -13,6 +13,31 @@ const MyPage = () => {
       </div>
       <div className="my-review-container">
         <span className="my-review-title top-title">내가 쓴 후기</span>
+        <div className="review-section">
+          <div className="comment-part">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quam
+            ligula, consectetur vitae est ac, euismod porttitor turpis.
+            Pellentesque sagittis nisi nec cursus fermentum.{" "}
+          </div>
+          <div className="control-part">
+            <span className="date-info">2024.01.01</span>
+            <button className="edit-btn">수정</button>
+            <button className="delete-btn">삭제</button>
+          </div>
+        </div>
+
+        <div className="review-section">
+          <div className="comment-part">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quam
+            ligula, consectetur vitae est ac, euismod porttitor turpis.
+            Pellentesque sagittis nisi nec cursus fermentum.{" "}
+          </div>
+          <div className="control-part">
+            <span className="date-info">2024.01.01</span>
+            <button className="edit-btn">수정</button>
+            <button className="delete-btn">삭제</button>
+          </div>
+        </div>
       </div>
       <div className="saved-loc-container">
         <span className="my-loc-title top-title">저장한 여행지</span>
