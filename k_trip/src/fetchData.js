ee665270854a97369221289788edfd9b0e55ac3e
박진영 <am030813@naver.com>
2024-06-28 18:00:22 +0900

    FIX: 서버 요청 함수 수정

--- k_trip/src/fetchData.js
+++ k_trip/src/fetchData.js
@@ -1,22 +1,37 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseURL = "http://localhost:8080/";
+const baseURL = 'http://localhost:8080/';
 
 const fetchData = async (endpoint, setData, setError, setLoading, params) => {
     try {
         setError(null);
         setLoading(true);
+        if(endpoint === 'signIn' || endpoint === 'mypage'){
+            const accessToken = localStorage.getItem('accessToken');
+            const response = await axios.get(`${baseURL}${endpoint}`, {
+                params,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': accessToken ? `${accessToken}` : ''
+                },
+                withCredentials: true
+            });
+            const dataArray = response.data;
+            setData(dataArray); // 상태 업데이트
+        }
+        else{
+            const response = await axios.get(`${baseURL}${endpoint}`, { params });
+            // Assuming the array you need is in response.data
+            const dataArray = response.data;
+            setData(dataArray); // Update the state with the fetched data
+            console.log(dataArray);
+        }
 
-        const response = await axios.get(`${baseURL}${endpoint}`, { params });
-        // Assuming the array you need is in response.data
-        const dataArray = response.data;
-        setData(dataArray); // Update the state with the fetched data
-        console.log(dataArray);
-    } catch (e) {
-        setError(e);
+    } catch (error) {
+        setError(error);
     } finally {
         setLoading(false);
     }
 };
-
+const accessToken = document.Authorization
 export default fetchData;
--- k_trip/src/postData.js
+++ k_trip/src/postData.js
@@ -1,13 +1,42 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseURL = "http://localhost:8080/";
+const baseURL = 'http://localhost:8080/';
 
 const postData = async (endpoint, setLoading, params) => {
     try {
+        setLoading(true);
         const url = `${baseURL}${endpoint}`;
-        await axios.post(url, null, {
-            params: params
-        });
+
+        if (endpoint === 'signIn') {
+            const response = await axios.post(url, params, {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                withCredentials: true
+            });
+            console.log('Response:', response.data);
+            const accessToken = response.headers['authorization'];
+
+
+            if (accessToken) {
+                localStorage.setItem('accessToken', accessToken);
+            }
+        } else if (endpoint === 'signUp') {
+            const response = await axios.post(url, params, {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                withCredentials: true
+            });
+            console.log('Response:', response.data);
+        } else {
+            const response = await axios.post(url, null, {
+                params: params
+            });
+            console.log('Response:', response.data);
+        }
+    } catch (error) {
+        console.error('Error posting data:', error);
     } finally {
         setLoading(false);
     }
