61f234a0c8dc3914938169a0b6115039645ae929
박진영 <am030813@naver.com>
2024-06-29 23:53:33 +0900

    FIX: 로그인 상태에 따른 예외처리

--- k_trip/src/pages/MyPage.js
+++ k_trip/src/pages/MyPage.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./my-page.css";
 import fetchData from "../fetchData";
-import auth from "../auth";
 
 function MyPage(){
     const navigate = useNavigate();
